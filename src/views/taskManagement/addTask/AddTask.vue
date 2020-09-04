955528c371021a82933c8a180a4de0fe6b66ff80
SNT202006001-CD <SNT202006001@DESKTOP-NMFB272>
2020-09-04 12:00:06 +0800

    格式修改

--- src/views/taskManagement/addTask/AddTask.vue
+++ src/views/taskManagement/addTask/AddTask.vue
@@ -1,175 +1,243 @@
 <template>
-	<div class="addTask-box dialog-box button-box">
-		<el-dialog title="新增任务" :visible.sync="dialogAdd">
-			<div class="content-box form-box">
-				<div class="cancel-box" @click="closeAdd"><i class="el-dialog__close el-icon el-icon-close"></i></div>
-				<el-form :model="addForm">
-					<div class="list-item">
-						<el-form-item class="has-two-item" label="任务名称：" label-width="120px" prop="taskName" :rules="[{ required: true, message: '任务名称不能为空' }]">
-							<div class="list-item-content-box">
-								<el-input type="taskName" v-model="addForm.taskName" autocomplete="off"></el-input>
-							</div>
-						</el-form-item>
-						<el-form-item class="has-two-item" label="任务负责人：" label-width="120px">
-							<el-button class="choose-active" type="primary" plain v-if="addForm.inCharge != ''" v-model="addForm.inCharge">{{ addForm.inCharge }}</el-button>
-							<el-button type="primary" plain @click="choosePerson">选择人员</el-button>
-						</el-form-item>
-					</div>
-					<div class="list-item">
-						<el-form-item class="has-two-item" label="开始时间：" label-width="120px">
-							<div class="list-item-content-box">
-								<el-date-picker v-model="addForm.estimatedStartTime" type="datetime" placeholder="预计任务开始时间"></el-date-picker>
-							</div>
-						</el-form-item>
-						<el-form-item class="has-two-item" label="预计结束时间：" label-width="120px">
-							<div class="list-item-content-box">
-								<el-date-picker v-model="addForm.estimatedEndTime" type="datetime" placeholder="预计任务结束时间"></el-date-picker>
-							</div>
-						</el-form-item>
-					</div>
-					<div class="list-item">
-						<el-form-item
-							class="has-two-item"
-							label="任务类别："
-							label-width="120px"
-							prop="taskType"
-							:rules="[{ required: true, message: '任务类别不能为空', trigger: 'change' }]"
-						>
-						<div class="list-item-content-box">
-							<el-select v-model="addForm.taskType" placeholder="请选择任务类别">
-								<el-option label="普通任务" value="puTong"></el-option>
-								<el-option label="临时任务" value="liShi"></el-option>
-							</el-select>
-						</div>
-						</el-form-item>
-						<el-form-item class="has-two-item" label="巡检片区：" label-width="120px">
-							<div class="list-item-content-box">
-								<!-- <el-input type="inspectionArea" v-model="addForm.inspectionArea" autocomplete="off"></el-input> -->
-								<el-button type="primary" plain @click="chooseArea">选择片区</el-button>
-							</div>
-						</el-form-item>
-					</div>
-					<el-form-item label="备注：" label-width="120px"><el-input type="textarea" :rows="3" v-model="addForm.remarks" autocomplete="off"></el-input></el-form-item>
-				</el-form>
-			</div>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click="closeAdd">取 消</el-button>
-				<el-button type="primary" @click="closeAdd">确 定</el-button>
-			</div>
-		</el-dialog>
+  <div class="addTask-box dialog-box button-box">
+    <el-dialog title="新增任务" :visible.sync="dialogAdd">
+      <div class="content-box form-box">
+        <div class="cancel-box" @click="closeAdd">
+          <i class="el-dialog__close el-icon el-icon-close"></i>
+        </div>
+        <el-form :model="addForm">
+          <div class="list-item">
+            <el-form-item
+              class="has-two-item"
+              label="任务名称："
+              label-width="120px"
+              prop="taskName"
+              :rules="[{required: true, message: '任务名称不能为空'}]"
+            >
+              <div class="list-item-content-box">
+                <el-input
+                  type="taskName"
+                  v-model="addForm.taskName"
+                  autocomplete="off"
+                ></el-input>
+              </div>
+            </el-form-item>
+            <el-form-item
+              class="has-two-item"
+              label="任务负责人："
+              label-width="120px"
+            >
+              <el-button
+                class="choose-active"
+                type="primary"
+                plain
+                v-if="addForm.inCharge != ''"
+                v-model="addForm.inCharge"
+                >{{ addForm.inCharge }}</el-button
+              >
+              <el-button type="primary" plain @click="choosePerson"
+                >选择人员</el-button
+              >
+            </el-form-item>
+          </div>
+          <div class="list-item">
+            <el-form-item
+              class="has-two-item"
+              label="开始时间："
+              label-width="120px"
+            >
+              <div class="list-item-content-box">
+                <el-date-picker
+                  v-model="addForm.estimatedStartTime"
+                  type="datetime"
+                  placeholder="预计任务开始时间"
+                ></el-date-picker>
+              </div>
+            </el-form-item>
+            <el-form-item
+              class="has-two-item"
+              label="预计结束时间："
+              label-width="120px"
+            >
+              <div class="list-item-content-box">
+                <el-date-picker
+                  v-model="addForm.estimatedEndTime"
+                  type="datetime"
+                  placeholder="预计任务结束时间"
+                ></el-date-picker>
+              </div>
+            </el-form-item>
+          </div>
+          <div class="list-item">
+            <el-form-item
+              class="has-two-item"
+              label="任务类别："
+              label-width="120px"
+              prop="taskType"
+              :rules="[
+                {required: true, message: '任务类别不能为空', trigger: 'change'}
+              ]"
+            >
+              <div class="list-item-content-box">
+                <el-select
+                  v-model="addForm.taskType"
+                  placeholder="请选择任务类别"
+                >
+                  <el-option label="普通任务" value="puTong"></el-option>
+                  <el-option label="临时任务" value="liShi"></el-option>
+                </el-select>
+              </div>
+            </el-form-item>
+            <el-form-item
+              class="has-two-item"
+              label="巡检片区："
+              label-width="120px"
+            >
+              <div class="list-item-content-box">
+                <!-- <el-input type="inspectionArea" v-model="addForm.inspectionArea" autocomplete="off"></el-input> -->
+                <el-button type="primary" plain @click="chooseArea"
+                  >选择片区</el-button
+                >
+              </div>
+            </el-form-item>
+          </div>
+          <el-form-item label="备注：" label-width="120px"
+            ><el-input
+              type="textarea"
+              :rows="3"
+              v-model="addForm.remarks"
+              autocomplete="off"
+            ></el-input
+          ></el-form-item>
+        </el-form>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="closeAdd">取 消</el-button>
+        <el-button type="primary" @click="closeAdd">确 定</el-button>
+      </div>
+    </el-dialog>
 
-		<choose-people :dialogCharge="dialogCharge" @closeChoosePeople="closeChoosePeople" @checkedPerson="checkedPerson"></choose-people>
-		<choose-area :dialogArea="dialogArea" @closeChooseArea="closeChooseArea" @checkedArea="checkedArea"></choose-area>
-	</div>
+    <choose-people
+      :dialogCharge="dialogCharge"
+      @closeChoosePeople="closeChoosePeople"
+      @checkedPerson="checkedPerson"
+    ></choose-people>
+    <choose-area
+      :dialogArea="dialogArea"
+      @closeChooseArea="closeChooseArea"
+      @checkedArea="checkedArea"
+    ></choose-area>
+  </div>
 </template>
 
 <script>
 import ChoosePeople from './ChoosePeople.vue';
 import ChooseArea from './ChooseArea.vue';
 export default {
-	name: 'AddTask',
-	props: ['dialogAdd'],
-	components: {
-		ChoosePeople,
-		ChooseArea
-	},
-	data() {
-		return {
-			addForm: {
-				taskName: '',
-				inCharge: '测试人员',
-				estimatedStartTime: '',
-				estimatedEndTime: '',
-				taskType: '',
-				inspectionArea: '',
-				remarks: ''
-			},
-			// 负责人弹窗状态
-			dialogCharge: false,
-			// 巡检片区弹窗状态
-			dialogArea: false
-		};
-	},
-	methods: {
-		// 点击取消或者右上角的×关闭新增弹窗
-		closeAdd() {
-			let data = {
-				dialogAdd: false,
-				data: []
-			};
-			this.$emit('getAddData', data);
-		},
-		// 点击选择负责人按钮
-		choosePerson() {
-			this.dialogCharge = true;
-		},
-		// 关闭选择负责人弹窗
-		closeChoosePeople(data) {
-			this.dialogCharge = data.dialogCharge;
-		},
-		// 选择负责人弹窗选择了负责人并点击了确定按钮
-		checkedPerson(data) {
-			console.log(data);
-			this.dialogCharge = data.dialogCharge;
-			this.addForm.inCharge = data.name;
-		},
-		// 点击选择片区按钮
-		chooseArea() {
-			this.dialogArea = true
-		},
-		// 关闭选择片区弹窗
-		closeChooseArea(data) {
-			this.dialogArea = data.dialogArea;
-		},
-		// 选择片区弹窗选择了片区并点击了确定按钮
-		checkedArea(data) {
-			console.log(data);
-			this.dialogArea = data.dialogArea;
-			// this.addForm.inCharge = data.name;
-		},
-	}
+  name: 'AddTask',
+  props: ['dialogAdd'],
+  components: {
+    ChoosePeople,
+    ChooseArea
+  },
+  data() {
+    return {
+      addForm: {
+        taskName: '',
+        inCharge: '测试人员',
+        estimatedStartTime: '',
+        estimatedEndTime: '',
+        taskType: '',
+        inspectionArea: '',
+        remarks: ''
+      },
+      // 负责人弹窗状态
+      dialogCharge: false,
+      // 巡检片区弹窗状态
+      dialogArea: false
+    };
+  },
+  methods: {
+    // 点击取消或者右上角的×关闭新增弹窗
+    closeAdd() {
+      let data = {
+        dialogAdd: false,
+        data: []
+      };
+      this.$emit('getAddData', data);
+    },
+    // 点击选择负责人按钮
+    choosePerson() {
+      this.dialogCharge = true;
+    },
+    // 关闭选择负责人弹窗
+    closeChoosePeople(data) {
+      console.log(data);
+      this.dialogCharge = data.dialogCharge;
+    },
+    // 选择负责人弹窗选择了负责人并点击了确定按钮
+    checkedPerson(data) {
+      console.log(data);
+      this.dialogCharge = data.dialogCharge;
+      this.addForm.inCharge = data.name;
+    },
+    // 点击选择片区按钮
+    chooseArea() {
+      this.dialogArea = true;
+    },
+    // 关闭选择片区弹窗
+    closeChooseArea(data) {
+      this.dialogArea = data.dialogArea;
+    },
+    // 选择片区弹窗选择了片区并点击了确定按钮
+    checkedArea(data) {
+      console.log(data);
+      this.dialogArea = data.dialogArea;
+      // this.addForm.inCharge = data.name;
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
-@import '../../../styles/element-ui-new.scss';
+@import '@/styles/element-ui-new.scss';
 
 .addTask-box {
-	.content-box {
-		position: relative;
+  .content-box {
+    position: relative;
 
-		.cancel-box {
-			width: 20px;
-			height: 20px;
-			background: #4b77be;
-			position: absolute;
-			top: -42px;
-			right: 0;
-			z-index: 20;
-			color: #ffffff;
-			font-size: 20px;
-		}
+    .cancel-box {
+      width: 20px;
+      height: 20px;
+      background: #4b77be;
+      position: absolute;
+      top: -42px;
+      right: 0;
+      z-index: 20;
+      color: #ffffff;
+      font-size: 20px;
+    }
 
-		.list-item {
-			display: flex;
-			justify-content: space-between;
+    .list-item {
+      display: flex;
+      justify-content: space-between;
 
-			.has-two-item {
-				width: 46%;
+      .has-two-item {
+        width: 46%;
 
-				.choose-active {
-					color: #ffffff;
-					background: #4b77be;
-					border: none;
-					padding: 7px 6px;
-					cursor: auto;
-				}
-				
-				.list-item-content-box {
-					width: 220px;
-				}
-			}
-		}
-	}
+        .choose-active {
+          color: #ffffff;
+          background: #4b77be;
+          border: none;
+          padding: 7px 6px;
+          cursor: auto;
+        }
+
+        .list-item-content-box {
+          width: 220px;
+        }
+      }
+    }
+  }
 }
 </style>
--- src/views/taskManagement/addTask/ChoosePeople.vue
+++ src/views/taskManagement/addTask/ChoosePeople.vue
@@ -1,188 +1,189 @@
 <template>
-	<div class="choosePeople-box dialog-box">
-		<el-dialog title="选择人员" :visible.sync="dialogCharge">
-			<div class="content-box">
-				<div class="cancel-box" @click="closeChoosePeople"><i class="el-dialog__close el-icon el-icon-close"></i></div>
-				<div class="search-box">
-					<el-input placeholder="请输入内容" prefix-icon="el-icon-search" v-model="searchWords"></el-input>
-					<el-button class="search-button" type="primary">查询</el-button>
-				</div>
-				<div class="table-box">
-					<el-table
-						ref="multipleTable"
-						:data="tableData"
-						:stripe="true"
-						tooltip-effect="dark"
-						height="400"
-						style="width: 100%"
-						:highlight-current-row="true"
-						@row-click="clickRow"
-					>
-						<el-table-column prop="name" label="姓名"></el-table-column>
-						<el-table-column prop="tel" label="联系方式"></el-table-column>
-					</el-table>
-				</div>
-				<page :pageData="[30, 40, 50, 100]" :total="400" @changePageSize="changePageSize" @changeCurrentPage="changeCurrentPage"></page>
-			</div>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click="closeChoosePeople">取 消</el-button>
-				<el-button type="primary" @click="determine">确 定</el-button>
-			</div>
-		</el-dialog>
-	</div>
+  <div class="choosePeople-box dialog-box button-box">
+    <el-dialog title="选择人员" :visible.sync="dialogCharge">
+      <div class="content-box">
+        <div class="cancel-box" @click="closeChoosePeople">
+          <i class="el-dialog__close el-icon el-icon-close"></i>
+        </div>
+        <div class="search-box">
+          <el-input
+            placeholder="请输入内容"
+            prefix-icon="el-icon-search"
+            v-model="searchWords"
+          ></el-input>
+          <el-button class="search-button" type="primary">查询</el-button>
+        </div>
+        <div class="table-box">
+          <el-table
+            ref="multipleTable"
+            :data="tableData"
+            :stripe="true"
+            tooltip-effect="dark"
+            height="400"
+            style="width: 100%"
+            :highlight-current-row="true"
+            @row-click="clickRow"
+          >
+            <el-table-column prop="name" label="姓名"></el-table-column>
+            <el-table-column prop="tel" label="联系方式"></el-table-column>
+          </el-table>
+        </div>
+        <page
+          :pageData="[30, 40, 50, 100]"
+          :total="400"
+          @changePageSize="changePageSize"
+          @changeCurrentPage="changeCurrentPage"
+        ></page>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="closeChoosePeople">取 消</el-button>
+        <el-button type="primary" @click="determine">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 import Page from '@/components/page/Page.vue';
 export default {
-	name: 'ChoosePeople',
-	props: ['dialogCharge'],
-	components: {
-		Page
-	},
-	data() {
-		return {
-			searchWords: '',
-			tableData: [
-				{
-					name: '测试人员1',
-					tel: '12345678910'
-				},
-				{
-					name: '测试人员2',
-					tel: ''
-				},
-				{
-					name: '测试人员3',
-					tel: ''
-				},
-				{
-					name: '测试人员4',
-					tel: ''
-				},
-				{
-					name: '测试人员5',
-					tel: ''
-				},
-				{
-					name: '测试人员6',
-					tel: ''
-				},
-				{
-					name: '测试人员7',
-					tel: ''
-				},
-				{
-					name: '测试人员8',
-					tel: ''
-				},
-				{
-					name: '测试人员9',
-					tel: ''
-				},
-				{
-					name: '测试人员10',
-					tel: ''
-				},
-				{
-					name: '测试人员11',
-					tel: ''
-				},
-				{
-					name: '测试人员12',
-					tel: ''
-				},
-				{
-					name: '测试人员13',
-					tel: ''
-				},
-				{
-					name: '测试人员14',
-					tel: ''
-				},
-				{
-					name: '测试人员15',
-					tel: ''
-				}
-			],
-			checkedName: ''
-		};
-	},
-	methods: {
-		closeChoosePeople() {
-			let data = {
-				dialogCharge: false
-			}
-			this.$emit('closeChoosePeople', data);
-		},
-		// 选中的行
-		clickRow(val) {
-			console.log(val);
-			this.checkedName = val.name
-		},
-		// 点击确定
-		determine(){
-			if(this.checkedName == ''){
-				alert('请选择负责人！')
-				return
-			}else{
-				let data = {
-					name: this.checkedName,
-					dialogCharge: false
-				}
-				
-				this.$emit('checkedPerson',data)
-			}
-		},
-		// 获取从分页传过来的每页多少条数据
-		changePageSize(data) {
-			console.log(data);
-		},
-		// 获取从分页传过来的当前页数
-		changeCurrentPage(data) {
-			console.log(data);
-		}
-	},
-	mounted() {}
+  name: 'ChoosePeople',
+  props: ['dialogCharge'],
+  components: {
+    Page
+  },
+  data() {
+    return {
+      searchWords: '',
+      tableData: [
+        {
+          name: '测试人员1',
+          tel: '12345678910'
+        },
+        {
+          name: '测试人员2',
+          tel: ''
+        },
+        {
+          name: '测试人员3',
+          tel: ''
+        },
+        {
+          name: '测试人员4',
+          tel: ''
+        },
+        {
+          name: '测试人员5',
+          tel: ''
+        },
+        {
+          name: '测试人员6',
+          tel: ''
+        },
+        {
+          name: '测试人员7',
+          tel: ''
+        },
+        {
+          name: '测试人员8',
+          tel: ''
+        },
+        {
+          name: '测试人员9',
+          tel: ''
+        },
+        {
+          name: '测试人员10',
+          tel: ''
+        },
+        {
+          name: '测试人员11',
+          tel: ''
+        },
+        {
+          name: '测试人员12',
+          tel: ''
+        },
+        {
+          name: '测试人员13',
+          tel: ''
+        },
+        {
+          name: '测试人员14',
+          tel: ''
+        },
+        {
+          name: '测试人员15',
+          tel: ''
+        }
+      ],
+      checkedName: ''
+    };
+  },
+  methods: {
+    closeChoosePeople() {
+      let data = {
+        dialogCharge: false
+      };
+      this.$emit('closeChoosePeople', data);
+    },
+    // 选中的行
+    clickRow(val) {
+      console.log(val);
+      this.checkedName = val.name;
+    },
+    // 点击确定
+    determine() {
+      if (this.checkedName == '') {
+        alert('请选择负责人！');
+        return;
+      } else {
+        let data = {
+          name: this.checkedName,
+          dialogCharge: false
+        };
+
+        this.$emit('checkedPerson', data);
+      }
+    },
+    // 获取从分页传过来的每页多少条数据
+    changePageSize(data) {
+      console.log(data);
+    },
+    // 获取从分页传过来的当前页数
+    changeCurrentPage(data) {
+      console.log(data);
+    }
+  },
+  mounted() {}
 };
 </script>
 
 <style scoped lang="scss">
+@import '@/styles/public.scss';
 .content-box {
-	position: relative;
+  .search-box {
+    display: flex;
 
-	.cancel-box {
-		width: 20px;
-		height: 20px;
-		background: #4b77be;
-		position: absolute;
-		top: -42px;
-		right: 0;
-		z-index: 20;
-		color: #ffffff;
-		font-size: 20px;
-	}
+    .el-input {
+      width: 200px;
+    }
 
-	.search-box {
-		display: flex;
+    .search-button {
+      margin-left: 5px;
+    }
+  }
 
-		.el-input {
-			width: 200px;
-		}
+  .table-box {
+    margin: 10px 0;
+    border: 1px solid #ddd;
+    box-sizing: border-box;
 
-		.search-button {
-			margin-left: 5px;
-		}
-	}
-
-	.table-box {
-		margin: 10px 0;
-		border: 1px solid #ddd;
-		box-sizing: border-box;
-
-		.el-table--striped .el-table__body tr.el-table__row--striped.el-table__row--striped.el-table__row--striped td {
-			background-color: #f5f5f5; 
-		}
-	}
+    .el-table--striped
+      .el-table__body
+      tr.el-table__row--striped.el-table__row--striped.el-table__row--striped
+      td {
+      background-color: #f5f5f5;
+    }
+  }
 }
 </style>
